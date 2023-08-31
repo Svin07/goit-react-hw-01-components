91c7aa4334062d60123ff3570a36b509c8c72ff3
Svin07 <svin07@ukr.net>
2023-08-31 22:06:48 +0300

    commit_4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import Profile from './Userprofile';
+import Statistics from './Statistics'
+import user from './user.json';
+import data from './data.json';
+
+
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+     
     >
-      Моя нова домашка
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+
+<Statistics />
     </div>
   );
 };
---
+++ src/components/Userprofile.js
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+
+export default function Profile({ username, tag, location, avatar, stats: { followers, views, likes } }) {
+    
+return (<div className="profile">
+<div className="description">
+  <img
+    src={avatar}
+    alt={username}
+    className="avatar"
+  />
+  <p className="name">{username}</p>
+  <p className="tag">{tag}</p>
+  <p className="location">{location}</p>
+</div>
+
+<ul className="stats">
+  <li>
+    <span className="label">Followers</span>
+    <span className="quantity">{followers}</span>
+  </li>
+  <li>
+    <span className="label">Views</span>
+    <span className="quantity">{views}</span>
+  </li>
+  <li>
+    <span className="label">Likes</span>
+    <span className="quantity">{likes}</span>
+  </li>
+</ul>
+</div>)
+
+
+}
+
+// Profile.PropTypes = {
+//     username: PropTypes.string,
+//     views: PropTypes.number,
+//     likes: PropTypes.number,
+//     followers: PropTypes.number,
+//     stats: PropTypes.shape({}),
+//     avatar: PropTypes.string,
+//     location: PropTypes.string,
+//     tag: PropTypes.string,
+// };
+
+// "username": "Jacques Gluke",
+// "tag": "jgluke",
+// "location": "Ocho Rios, Jamaica",
+// "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+// "stats": {
+//   "followers": 5603,
+//   "views": 4827,
+//   "likes": 1308
+// }
+
+// { username, tag, location, avatar, stats: { followers, views, likes } }
